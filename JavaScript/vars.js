980e7162d5ac283b333e1922ff56e861784c4d01
shanelperera <shanelradperera@gmail.com>
2025-07-05 17:40:11 +0530

    Add TinDog project files and initial implementation
    - Created index.html for the TinDog landing page with Bootstrap integration.
    - Added CSS files (solution.css and style.css) for styling the TinDog application.
    - Included various goal images for features, footer, pricing, and testimonials.
    - Added SVG logo (Paw_Print.svg) and other images for branding.
    - Implemented JavaScript files (switch.js and vars.js) for basic functionality.
    - Created a new section for Tropical Bliss with an embedded Canva design.
    - Added a link.txt file for easy access to the Tropical Bliss design link.

---
+++ JavaScript/vars.js
@@ -0,0 +1,10 @@
+// 1
+var myName = "Chrome"
+var yourName = prompt("What is your name?");
+alert("My name is " + myName + ", Welcome to the browser " + yourName + "!");
+
+// 2
+var gameLevel = 1;
+gameLevel = 2;
+gameLevel = 3;
+alert("Your current game level is: " + gameLevel);
