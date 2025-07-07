d03f5f64c4e3a284b81a6ff3429871dced79f75a
shanelperera <shanelradperera@gmail.com>
2025-07-07 11:56:09 +0530

    Add love calculator and random number generator scripts

---
+++ JavaScript/love_calc.js
@@ -0,0 +1,23 @@
+function getRandomNum(max) {
+    return Math.floor(Math.random() * max) + 1;
+}
+
+var maxPercentage = 100;
+
+var name1 = prompt("Enter name 1 ");
+var name2 = prompt("Enter name 2 ");
+
+var yourLoveScore = getRandomNum(maxPercentage);
+
+if (yourLoveScore > 75) {
+    alert(name1.slice(0, 1).toUpperCase() + name1.slice(1, name1.length).toLowerCase() + "❤️" + 
+    name2.slice(0, 1).toUpperCase() + name2.slice(1, name2.length).toLowerCase() + " : " + yourLoveScore + 
+     "%. You love each other like Kanye loves Kanye.");
+}
+else {
+    alert(name1.slice(0, 1).toUpperCase() + name1.slice(1, name1.length).toLowerCase() + "❤️" + 
+    name2.slice(0, 1).toUpperCase() + name2.slice(1, name2.length).toLowerCase() + " : " + yourLoveScore + 
+     "%.");
+}
+
+
---
+++ JavaScript/random.js
@@ -0,0 +1,4 @@
+var n = Math.random()
+n *= 6;
+n = Math.floor(n) + 1;
+console.log(n);
