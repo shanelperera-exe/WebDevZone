9a2442f5f143602283a3dccde97733cc43712966
shanelperera <shanelradperera@gmail.com>
2025-08-08 16:25:01 +0530

    Add new react exercises and JS ES6 exercises

--- React/changing-complex-state/src/components/App.jsx
+++ React/changing-complex-state/src/components/App.jsx
@@ -1,16 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName
+        }
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value
+        }
+      }
+    })
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input 
+          name="fName" 
+          placeholder="First Name" 
+          onChange={handleChange}
+          value={fullName.fName}
+        />
+        <input 
+          name="lName" 
+          placeholder="Last Name" 
+          onChange={handleChange}
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
   );
 }
+
+// function App() {
+//   const [fName, setFirstName] = useState("");
+//   const [lName, setLastName] = useState("");
+
+//   function updateFName(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function updateLName(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   return (
+//     <div className="container">
+//       <h1>Hello {fName} {lName}</h1>
+//       <form>
+//         <input 
+//           name="fName" 
+//           placeholder="First Name" 
+//           onChange={updateFName}
+//           value={fName}
+//         />
+//         <input 
+//           name="lName" 
+//           placeholder="Last Name" 
+//           onChange={updateLName}
+//           value={lName}
+//         />
+//         <button>Submit</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 export default App;
